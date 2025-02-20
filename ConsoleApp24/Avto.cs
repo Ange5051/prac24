8fc3922344b97e956971c938b58c6c4e9bb70b5d
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-02-20 11:01:23 +0200

    Add files via upload

---
+++ ConsoleApp24/Avto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Avto
+    {
+        
+            private string brand;
+        private string color;
+        private int skor;
+
+        public int Skor
+        {
+            get { return skor; }
+            set
+            {
+                if (value < 20)
+                    skor = 20;
+                else if (value > 120)
+                    skor = 120;
+                else skor = value;
+            }
+
+        }
+        public string Brand
+        {
+            get
+            { return brand; }
+            set
+            { brand = value; }
+
+        }
+
+        public string Color { get; internal set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Марка: {brand}, Цвет: {color}, Скорость: {skor} км/ч");
+        }
+    }
+}
---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto car1 = new Avto();
+            car1.PrintInfo();
+
+            car1.Brand = "Toyota LandCruser 400";
+            car1.Color = "Черный";
+            car1.Skor = 100;
+            car1.PrintInfo();
+
+      
+            Console.ReadKey();
+
+        }
+    }
+}
